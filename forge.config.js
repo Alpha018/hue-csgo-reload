30dbffd28c98d4fb609670f151fd8da0107855fa
Alpha018 <tas001@alumnos.ucn.cl>
2021-05-19 21:28:59 -0400

    feat: implement minor configuration to make exe file

--- forge.config.js
+++ forge.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   makers: [
     {
-      name: '@electron-forge/maker-wix',
-      config: {
-        language: 1033,
-        manufacturer: 'My Awesome Company'
-      }
-    },
-    {
       name: '@electron-forge/maker-squirrel',
       config: {}
     }
