7f2312dee74521bcf5675d3e9ce272bd1e3e44d6
Alpha018 <tas001@alumnos.ucn.cl>
2021-05-19 23:23:14 -0400

    feat: publish command

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'Alpha018',
+          name: 'hue-csgo-reload'
+        }
+      }
+    }
+  ],
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
